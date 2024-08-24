71f1d8a0686ddb4b0bb5060e933b34030c92ad0c
Khabir Uddin <khuddin@csc.fi>
2024-08-24 15:00:34 +0300

    hosts specific node defination

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,26 @@
 node default {
 }
+
+node 'sys-ubu20-dev1' {
+  include infra
+}
+
+node 'sys-ubu20-dev2' {
+  include infra
+}
+
+node 'sys-ubu20-prod' {
+  include infra
+}
+
+node 'sys-ubu22-dev1' {
+  include infra
+}
+
+node 'sys-ubu22-dev2' {
+  include infra
+}
+
+node 'sys-ubu22-prod' {
+  include infra
+}
